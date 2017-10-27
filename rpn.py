08d7a76416fb6ea75145b41acb8f1ca11e8fdb04
Yi Gu <yigu@umich.edu>
2017-10-27 12:19:19 -0400

    Implemented ADD (+)

--- rpn.py
+++ rpn.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
-def calculate(myarg):
-    pass
 
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        if token == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+    print(stack)
+    return stack.pop()
 def main():
     while True:
-	calculate(input("rpn calc> "))
+        calculate(input("rpn calc> "))
 
 if __name__ == '__main__':
     main()
