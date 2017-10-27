fa43127347848a0777ffa48040563db8893a7801
Yi Gu <yigu@umich.edu>
2017-10-27 12:37:13 -0400

    Modified the operator

--- rpn.py
+++ rpn.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
+import operator
+
+ops = {
+	'+': operator.add,
+	'-': operator.sub,
+}
 
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
-        if token == '+':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 + arg2
-            stack.append(result)
-        elif token == '-':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 - arg2
-            stack.append(result)
-        else:
-            stack.append(int(token))
-    print(stack)
+    	try:
+    		stack.append(int(token))
+    	except ValueError:
+    		arg2 = stack.pop()
+    		arg1 = stack.pop()
+    		function  = ops[token]
+    		result = function(arg1,arg2)
+    		stack.append(result)
+    #print(stack)
     return stack.pop()
-    
+
 def main():
     while True:
         calculate(input("rpn calc> "))
