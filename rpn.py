354fb0847140660378eb1cda3b3ac0bc9a612cac
Yi Gu <yigu@umich.edu>
2017-10-27 12:28:31 -0400

    Implement subtract

--- rpn.py
+++ rpn.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
-
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
         if token == '+':
-            arg1 = stack.pop()
             arg2 = stack.pop()
+            arg1 = stack.pop()
             result = arg1 + arg2
+            stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
             stack.append(result)
         else:
             stack.append(int(token))
     print(stack)
     return stack.pop()
+    
 def main():
     while True:
         calculate(input("rpn calc> "))
